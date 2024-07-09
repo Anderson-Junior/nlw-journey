42df515a7ca6a1127e5a4b372b538ac2e6e4d181
Anderson-Junior <ander.lemos.jr@gmail.com>
2024-07-08 22:12:03 -0300

    Initial commit

---
+++ src/Journey.Api/Controllers/TripsController.cs
@@ -0,0 +1,44 @@
+﻿using Journey.Application.UseCases.GetAll;
+using Journey.Application.UseCases.Trips.Register;
+using Journey.Communication.Requests;
+using Journey.Exception;
+using Journey.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journey.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register([FromBody] RequestRegisterTripJson request)
+        {
+            try
+            {
+                var userCase = new RegisterTripUserCase();
+                var response = userCase.Execute(request);
+
+                return Created(string.Empty, response);
+            }
+            catch (JourneyException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ResourceErrorMessages.UNKNOWN_ERROR);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userCase = new GetAllTripsUseCase();
+
+            var result = userCase.Execute();
+
+            return Ok(result);
+        }
+    }
+}
